6c7639575ffcaabe2464e3c132f4689cd4ab4267
Jason Eckstein <jekyllstein@gmail.com>
2017-09-11 20:38:08 -0600

    Update parallelLoopTests.jl
    Removed final test suite with workers exceeding Sys.CPU_CORES.  Seemed to be causing errors on the linux build system.

--- test/parallelLoopTests.jl
+++ test/parallelLoopTests.jl
@@ -146,68 +146,68 @@ end
 
 println("")
 
-##--------------------------------------------------------------------------------------------------------
-addprocs(Sys.CPU_CORES)
-@everywhere using ParallelProgressMeter
-println(string("Adding ", Sys.CPU_CORES, " additional workers and initializing package on them"))
+# ##--------------------------------------------------------------------------------------------------------
+# addprocs(Sys.CPU_CORES)
+# @everywhere using ParallelProgressMeter
+# println(string("Adding ", Sys.CPU_CORES, " additional workers and initializing package on them"))
 
-##----------------------------------Tests with number of CPU cores as added workers--------------------------------------------
+# ##----------------------------------Tests with number of CPU cores as added workers--------------------------------------------
 
-##Multiple task test
-println("")
-#define a generic serial task that will be run in parallel once for each CPU core
-#in order to push updates to the progressArray each serial task must have access to
-#the shared array keeping track of progress and take the task number as input
-@everywhere function serialTask3(N::Int64, taskNum::Int64, p::SharedArray{Int64, 1})
-    a = 0.0
-    for i = 1:N
-        a += rand()
-        @inbounds p[taskNum] += 1
-    end
-    return a
-end
+# ##Multiple task test
+# println("")
+# #define a generic serial task that will be run in parallel once for each CPU core
+# #in order to push updates to the progressArray each serial task must have access to
+# #the shared array keeping track of progress and take the task number as input
+# @everywhere function serialTask3(N::Int64, taskNum::Int64, p::SharedArray{Int64, 1})
+#     a = 0.0
+#     for i = 1:N
+#         a += rand()
+#         @inbounds p[taskNum] += 1
+#     end
+#     return a
+# end
 
-@everywhere function serialTaskLean3(N::Int64)
-    a = 0.0
-    for i = 1:N
-        a += rand()
-    end
-    return a
-end
+# @everywhere function serialTaskLean3(N::Int64)
+#     a = 0.0
+#     for i = 1:N
+#         a += rand()
+#     end
+#     return a
+# end
 
-println(string("Starting parallel for loop test with ", numTasks, " tasks across ", nprocs() -1, " workers"))
+# println(string("Starting parallel for loop test with ", numTasks, " tasks across ", nprocs() -1, " workers"))
 
-#initialize parallel progress monitor and save shared array
-p = initializeProgress(numTasks, params)
+# #initialize parallel progress monitor and save shared array
+# p = initializeProgress(numTasks, params)
 
-#run parallel for loop with progress meter
-@parallel (vcat) for i = 1:numTasks
-    serialTask3(params[i], i, p)
-end
+# #run parallel for loop with progress meter
+# @parallel (vcat) for i = 1:numTasks
+#     serialTask3(params[i], i, p)
+# end
 
-println("Multi task run without progress meter")
-#run parallel for loop without
-@time @parallel (vcat) for i = 1:numTasks
-    serialTaskLean3(params[i])
-end
+# println("Multi task run without progress meter")
+# #run parallel for loop without
+# @time @parallel (vcat) for i = 1:numTasks
+#     serialTaskLean3(params[i])
+# end
 
-##Single task test
+# ##Single task test
 
-println("")
-println(string("Starting parallel for loop test for a single task with ", N, " iterations across ", nprocs() - 1, " workers"))
-p = initializeProgress(N)
+# println("")
+# println(string("Starting parallel for loop test for a single task with ", N, " iterations across ", nprocs() - 1, " workers"))
+# p = initializeProgress(N)
 
-#the value being accumulated must be at the end of the loop code block but the next! line should 
-#appear after the desired computation
-a = @parallel (+) for i = 1:N
-    tmp = rand()
-    next!(p)
-    tmp
-end
+# #the value being accumulated must be at the end of the loop code block but the next! line should 
+# #appear after the desired computation
+# a = @parallel (+) for i = 1:N
+#     tmp = rand()
+#     next!(p)
+#     tmp
+# end
 
-println(string("Summed a uniform (0, 1) random variable ", N, " times in parallel across ", nprocs() - 1, " workers to a value of ", a))
+# println(string("Summed a uniform (0, 1) random variable ", N, " times in parallel across ", nprocs() - 1, " workers to a value of ", a))
 
-println("Single task run without progress meter")
-@time a = @parallel (+) for i = 1:N
-    tmp = rand()
-end
+# println("Single task run without progress meter")
+# @time a = @parallel (+) for i = 1:N
+#     tmp = rand()
+# end
